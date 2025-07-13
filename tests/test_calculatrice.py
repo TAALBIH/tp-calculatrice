a40a8b60ec1807c8402ad59acfe78e9e18271928
TAALBIH <taalbi.hichem@gmail.com>
2025-07-12 23:44:18 -0400

    1ere commit

--- tests/test_calculatrice.py
+++ tests/test_calculatrice.py
@@ -1,7 +1,12 @@
 
 import unittest
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from src.calculatrice import addition, soustraction, multiplication, division
 
+import unittest
 class TestCalculatrice(unittest.TestCase):
 
     def test_addition(self):
